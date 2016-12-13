488ded25e563b25ff994c8a75794708d5ef1d8c4
sseira <sseira@pa-m-co-37.ideo.com>
2016-12-13 14:24:56 -0800

    overlaying 3 data sets, determining marker size based on zoom

--- SFMap.js
+++ SFMap.js
@@ -1,17 +1,8 @@
 
 // ----------- Global Data ------------
-var shouldShowIntersections = false,
-    intersectionData,
-    shouldShowParkingMeters = false,
-    parkingMeterData,
-    shouldShowOffStreetParking = false,
-    offStreetParkingData
-
-
-
-
-
-
+var globalData = [], 
+    currentClassObj,
+    zoomLvl = 12
 
 
 // Create the Google Map…
@@ -30,7 +21,7 @@ function initMap() {
 
           //------------ initializing the map
         var map = new google.maps.Map(d3.select("#map").node(), {
-                zoom: 12,
+                zoom: zoomLvl,
                 center: new google.maps.LatLng(37.76487, -122.41948),
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 zoomControlOptions: {
@@ -47,7 +38,9 @@ function initMap() {
 
             // Remap data on every resize
             overlay.draw = function() {
-                setDataSets()
+                zoomLvl = map.getZoom()
+                console.log(zoomLvl)
+                drawAllData()
             }
         }
 
@@ -108,47 +101,109 @@ function initMap() {
     };
 
 
-    var setDataSets = function() {
-        var data
-        if (shouldShowIntersections) {
-            data = intersectionData['data']
-            getLocation = Intersection.location
-            getValue = Intersection.value
-            drawData(data)
-            addColors(data)
-        } else if (shouldShowParkingMeters) {
-            data = parkingMeterData['data']
-            getLocation = ParkingMeter.location
-            drawData(data)
-        } else if (shouldShowOffStreetParking) {
-            data = offStreetParkingData['data']
-            getLocation = OffStreetParking.location
-            getValue = OffStreetParking.value
-            drawData(data)
-        } else {
-            drawData()
+
+    // // doesnt scale
+    // // make data structure that has data, yes/no, type, functions
+    // var setDataSets = function() {
+    //     var data,
+    //         type
+    //     if (shouldShowIntersections) {
+    //         data = intersectionData['data']
+    //         getLocation = Intersection.location
+    //         getValue = Intersection.value
+    //         type = 'intersections'
+    //         drawData(data, type, Intersection)
+    //         addColorRange(data, type, Intersection)
+    //     } else {
+    //         type = 'intersections'
+    //         drawData(null, type)
+    //     }
+
+    //     if (shouldShowParkingMeters) {
+    //         data = parkingMeterData['data']
+    //         getLocation = ParkingMeter.location
+    //         type = 'parking-meters'
+    //         drawData(data, type, ParkingMeter)
+    //     } else {
+    //         type = 'parking-meters'
+    //         drawData(null, type)
+    //     } 
+
+    //     if (shouldShowOffStreetParking) {
+    //         data = offStreetParkingData['data']
+    //         getLocation = OffStreetParking.location
+    //         getValue = OffStreetParking.value
+    //         type = 'off-street-parking'
+    //         drawData(data, type, OffStreetParking)
+    //     } else {
+    //         type = 'off-street-parking'
+    //         drawData(null, type)
+    //     } 
+    // }
+
+
+
+    var drawAllData = function() {
+        for(var i=0; i<globalData.length; i++) {
+            var dataObj = globalData[i]
+
+            if(dataObj.shouldShow) {
+                drawData(dataObj.data, dataObj.type, dataObj.classObj)
+                
+                if (dataObj.type === 'intersection') {
+                    addColorRange(dataObj.data, dataObj.type)
+                } else {
+                    addColor(dataObj.data, dataObj.type, dataObj.color)
+                }
+            } else {
+                drawData(null, dataObj.type)
+            }
         }
     }
 
 
+    var getRadiusByZoom = function() {
+        if (zoomLvl < 12) {
+            return .5
+        } else if (zoomLvl < 13) {
+            return 1
+        } else if (zoomLvl < 14) {
+            return 3
+        } else if (zoomLvl < 15) {
+            return 4
+        } else if (zoomLvl < 16) {
+            return 5
+        } else {
+            return 6
+        }
+
+    }
+
     // eventually add multiple of overlapping layers of data 
+    // how to call class methods 
     //--------------- mapping data
-    var drawData = function(data) {
+    var drawData = function(data, type, classObj) {
+        currentClassObj = classObj
         if(!data) {
-            d3.select('.data-layer').selectAll("svg").remove()
+            d3.select('.data-layer').selectAll("svg."+type).remove()
             return
         }
 
-        var marker = d3.select('.data-layer').selectAll("svg")
+        var marker = d3.select('.data-layer').selectAll('.'+type)
             .data(d3.entries(data))
             .each(transformLocations) // update existing markers
             .enter().append("svg")
             .each(transformLocations)
-            .attr("class", "marker");
+            .attr("class", type);
+
+
+        // resize
+        d3.select('.data-layer').selectAll('.'+type).selectAll('circle')
+            .attr("r", getRadiusByZoom())
 
         // Add a circle.
         marker.append("circle")
-            .attr("r", 1.5)
+            .attr("r", getRadiusByZoom())
             .attr("cx", padding)
             .attr("cy", padding)
 
@@ -158,19 +213,28 @@ function initMap() {
             google.maps.event.trigger(this, 'click');
         });
 
-
+        getLocation = classObj.location
+        getValue = classObj.value
         marker.on('click', function() {
+            var classObj = currentClassObj
             var dataElement = d3.select(this).datum()
 
             console.log(getLocation(dataElement))
+            console.log(getValue(dataElement))
         });
     };
 
 
-    var addColors = function(data) {
-         d3.select('.data-layer').selectAll("svg")
+    var addColorRange = function(data, type) {
+         d3.select('.data-layer').selectAll("."+type)
             .data(d3.entries(data))
             .each(colorScale)
+    }
+
+    var addColor = function(data, type, color) {
+         d3.select('.data-layer').selectAll("."+type)
+            .data(d3.entries(data))
+            .attr('fill', color)
     }
 
     var colorScale = function(d) {
@@ -184,7 +248,7 @@ function initMap() {
 
 
     function transformLocations(d) {
-        var location = getLocation(d),
+        var location = currentClassObj.location(d),
             googleLocation = new google.maps.LatLng(location.lat, location.lng),
             mapLocation = overlay.getProjection().fromLatLngToDivPixel(googleLocation)
 
@@ -258,21 +322,26 @@ function initMap() {
         $("input[type='checkbox']").change(function() {
             $("input[type='checkbox']").each(function(index, element) {
                
-                if (element.id === 'intersections') {
-                    shouldShowIntersections = element.checked
-                }
-                
-                if (element.id === 'parking-meters') {
-                    shouldShowParkingMeters = element.checked
+                for(var i=0; i<globalData.length; i++) {
+                    var dataObj = globalData[i]
+                    if (element.id === dataObj.type) {
+                        dataObj.shouldShow = element.checked
+                    } 
                 }
 
-                if (element.id === 'off-street-parking') {
-                    shouldShowOffStreetParking = element.checked
-                }
+               
+                
+                // if (element.id === 'parking-meter') {
+                //     shouldShowParkingMeters = element.checked
+                // }
+
+                // if (element.id === 'off-street-parking') {
+                //     shouldShowOffStreetParking = element.checked
+                // }
                 // repeat for other datasets
 
             });
-            setDataSets()
+            drawAllData()
         });
     }()
 
@@ -283,6 +352,14 @@ function initMap() {
         d3.json("data/pedestrian_traffic_at_intersections.json", function(error, data) {
             if (error) throw error;
             intersectionData = data;
+
+
+            globalData.push({
+                data: data.data,
+                type: 'intersection',
+                classObj: Intersection,
+                shouldShow: false
+            })
         });
     }()
 
@@ -290,13 +367,29 @@ function initMap() {
         d3.json("data/parking_meters.json", function(error, data) {
             if (error) throw error;
             parkingMeterData = data;
+
+            globalData.push({
+                data: data.data,
+                type: 'parking-meter',
+                classObj: ParkingMeter,
+                shouldShow: false,
+                color: 'blue'
+            })
         });
     }()
     
     var loadOffStreetParkingData = function() {
         d3.json("data/off_street_parking.json", function(error, data) {
             if (error) throw error;
-            offStreetParking = data;
+            offStreetParkingData = data;
+
+            globalData.push({
+                data: data.data,
+                type: 'off-street-parking',
+                classObj: OffStreetParking,
+                shouldShow: false,
+                color: 'green'
+            })
         });
     }()
 
@@ -328,12 +421,12 @@ ParkingMeter.location = function(d) {
 
 function OffStreetParking () {}
 OffStreetParking.value = function(d) {
-    return d[13]
+    return d.value[13]
 }
 OffStreetParking.location = function(d) {
     return {
-        lat: d[17][1],
-        lng: d[17][2]
+        lat: d.value[17][1],
+        lng: d.value[17][2]
     }
 }
 
